7ba4ce02a3faf4c366cd5e8b4e5d152872be750d
Evan Payne <136827111+EvanPayne22@users.noreply.github.com>
2024-06-02 23:05:44 -0400

    Update run_sxdefectalign_code.sh

--- run_sxdefectalign_code.sh
+++ run_sxdefectalign_code.sh
@@ -11,9 +11,9 @@ for a in $directories
 do
 	cd $a
 	if [ -f "LOCPOT" ]; then 
-		#Gets Defect Location by using location specified in POSCAR header, ex. Supercell of Rb12 I_Sb at 0.4159365 0.6027021   0.4107527  
+		#Gets Defect Location by using location specified in POSCAR of the neutral charge state in the header, ex. Rb12 Sb3 I9 0.4159365 0.6027021 0.4107527  
 		nonChargeDefect="${a//[0-9\/-]/}"0
-		defectLocation=$(head -1 "../$nonChargeDefect/originalPOSCAR" | awk '{print $4 "," $5 "," $6}')
+		defectLocation=$(head -1 "../$nonChargeDefect/POSCAR" | awk '{print $4 "," $5 "," $6}')
 		charge=$(echo "$a" | tr -cd '0-9-')
 		# Check if the charge is negative
 		if [ "${charge:0:1}" = "-" ]; then
@@ -23,7 +23,7 @@ do
     			# If it's positive, add a minus sign to make it negative
     			charge="-$charge"
 		fi
-		#Runs the sxdefect code: change values/locations as needed
+		#Runs the sxdefect code: change values/locations as needed, you will also need to have sxdefectalign downloaded
 		~/work/CodeFiles/sxdefectalign --ecut 20 --charge $charge --tensor 10.874,10.674,10.263 --center $defectLocation --relative --vdef ~/work/RbSbI/ortho/supercell/"$a"LOCPOT --vref ~/work/RbSbI/ortho/supercell/Bulk/LOCPOT --vasp >> results.txt 
 		free_en=$(grep "free  en" OUTCAR | tail -1 | awk '{print $5}')
 		results=$(tail -1 "results.txt" | awk '{print $4}')

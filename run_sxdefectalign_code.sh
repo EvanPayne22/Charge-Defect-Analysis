947c50452b6f9ddd62e8fa4f83b8fb5f34ff75b5
Evan Payne <136827111+EvanPayne22@users.noreply.github.com>
2024-07-16 14:44:39 -0400

    Update run_sxdefectalign_code.sh

--- run_sxdefectalign_code.sh
+++ run_sxdefectalign_code.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
-
+  
 #Get Directories
-directories=$(ls -d */) 
+directories=$(ls -d */)
 
 rm ./energies_correction.csv
 
 echo "Defect Name, Bulk Energy, Correction Energy" >> ./energies_correction.csv
 
+bulk="../latticeRelax/"
+free_en=$(grep TOTEN "$bulk"OUTCAR | tail -1 | awk '{printf $5}')
+
+echo "bulk, $free_en, 0" >> ./energies_correction.csv
+
+
 for a in $directories
 do
-	cd $a
-	if [ -f "LOCPOT" ]; then 
-		#Gets Defect Location by using location specified in POSCAR of the neutral charge state in the header, ex. Rb12 Sb3 I9 0.4159365 0.6027021 0.4107527  
-		nonChargeDefect="${a//[0-9\/-]/}"0
-		defectLocation=$(head -1 "../$nonChargeDefect/POSCAR" | awk '{print $4 "," $5 "," $6}')
-		charge=$(echo "$a" | tr -cd '0-9-')
-		# Check if the charge is negative
-		if [ "${charge:0:1}" = "-" ]; then
-    			# If it's negative, remove the minus sign to make it positive
-    			charge="${charge:1}"
-		else
-    			# If it's positive, add a minus sign to make it negative
-    			charge="-$charge"
-		fi
-		#Runs the sxdefect code: change values/locations as needed, you will also need to have sxdefectalign downloaded
-		~/work/CodeFiles/sxdefectalign --ecut 20 --charge $charge --tensor 10.874,10.674,10.263 --center $defectLocation --relative --vdef ~/work/RbSbI/ortho/supercell/"$a"LOCPOT --vref ~/work/RbSbI/ortho/supercell/Bulk/LOCPOT --vasp >> results.txt 
-		free_en=$(grep "free  en" OUTCAR | tail -1 | awk '{print $5}')
-		results=$(tail -1 "results.txt" | awk '{print $4}')
+        cd $a
+        if [ -f "LOCPOT" ]; then
+                #Gets Defect Location by using location specified in POSCAR header, ex. Supercell of Rb12 I_Sb at 0.4159365 0.6027021   0.4107527  
+                string=${a: -3}
+                nonChargeDefect="${a: 0 : -3}""${string//[0-9\/-]/}"0
+                defectLocation=$(head -1 "../$nonChargeDefect/POSCAR" | awk '{print $3 "," $4 "," $5}')
+                charge=$(echo "$string" | tr -cd '0-9-')
+                # Check if the charge is negative
+                if [ "${charge:0:1}" = "-" ]; then
+                        # If it's negative, remove the minus sign to make it positive
+                        charge="${charge:1}"
+                else
+                        # If it's positive, add a minus sign to make it negative
+                        charge="-$charge"
+                fi
+                echo $charge
+                #Runs the sxdefect code: change values/locations as needed
+                ~/work/bin/sxdefectalign --ecut 30 --charge $charge --tensor 11.04,11.04,11.04  --center $defectLocation --relative --vdef ./LOCPOT --vref ../"$bulk"LOCPOT --vasp >> results.txt
+                free_en=$(grep "free  en" OUTCAR | tail -1 | awk '{print $5}')
+                results=$(tail -1 "results.txt" | awk '{print $4}')
 
-		echo "$a, $free_en, $results" >> ../energies_correction.csv
-		#rm results.txt
-	fi
-	cd ..
+                echo "$a, $free_en, $results" >> ../energies_correction.csv
+                #rm results.txt
+        fi
+        cd ..
 done

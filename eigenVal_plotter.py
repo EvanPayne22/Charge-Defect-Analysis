6b748f3893af16029a021e10d81efc185587d33f
Evan Payne <136827111+EvanPayne22@users.noreply.github.com>
2025-06-06 11:27:36 -0400

    Update eigenVal_plotter.py
    Remade script to have better formatting. and added comments.

--- eigenVal_plotter.py
+++ eigenVal_plotter.py
@@ -1,78 +1,79 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 25 15:07:04 2024
+Created on Wed Jun  4 00:52:18 2025
 
-@author: evanp
+@author: aheib
 """
 
 import matplotlib.pyplot as plt
 
-# Function to format labels with subscript
+#Function to format defect names with subscript
 def format_label(label):
     base, subscript = label.split('_')
     return f"{base}$_{{{subscript}}}$"
 
 data = r"./eigenVal.txt"
-# data = r"./eigenVal2.csv"
 
 f = open(data)
 eigenVal = f.readlines()
 
-for i in range(0, len(eigenVal)):
+numberOfDefects = 0 #The number of defects being plotted
+
+for i in range (0, len(eigenVal)):
+    if (eigenVal[i][0] == 'd'): #Countes the Number of Defects Being Plotted
+        numberOfDefects += 1
     eigenVal[i] = eigenVal[i].split()
+    
+    
+count = 0 #Counter for reading eigenVal file
+plotNum = 1 #Counter to store which sub plot data is stored for
 
-numberOfDefects = 9
+energy = [] #Energy Level of the Defect in energy Gap
+occupancy = [] #Occupancy of Defect States
+dotSize = 100 #Size of the dots on the plot
 
-band = []
-occupancy = []
+plt.subplots(1,numberOfDefects, figsize=(12,4)) #Sets the number of subplots
 
-i = 1
-k = 1
-total = 1
-
-dotSize = 100
-
-plt.subplots(1,numberOfDefects, figsize=(12,4))
-
-while (i < len(eigenVal)):
-    title = eigenVal[i][2]
-    title = format_label(title)
-    while(eigenVal[i]):
-        band.append(float(eigenVal[i][0]))
-        occupancy.append(float(eigenVal[i][1]))
-        total = total + 1
+for i in range (0, len(eigenVal)):
+    if(count == 0):
+        band_edge = float(eigenVal[i][1])
+    count = count + 1
+    if(eigenVal[i][0] == 'done'):
         i = i + 1
-
-    plt.subplot(1,numberOfDefects,k)
-    plt.tick_params(labelbottom=False,bottom=False, top=False, labeltop=False, labelright=False, labelleft=False, left=False, right=False)
-    vbm = band[0]
-    for j in range(1, len(band) - 1):
-        band[j] = band[j] - vbm
-        if(occupancy[j] < 0.2):
-            plt.axhline(band[j], color = 'black')
-            plt.scatter([0.25, 0.75], [band[j], band[j]], facecolors='white', edgecolors='black', zorder = 3, s = dotSize)
-        elif(occupancy[j] > 0.8):
-            plt.axhline(band[j], color = 'black')
-            plt.scatter([0.25, 0.75], [band[j], band[j]], color = 'black', zorder = 3, s = dotSize)
-        else:
-            filledState = float(eigenVal[i - (len(band) - j)][2]) - vbm
-            emptyState = float(eigenVal[i - (len(band) - j)][3]) - vbm
-            plt.axhline(filledState, color = 'black', xmax = 0.5)
-            plt.axhline(emptyState, color = 'black', xmin = 0.5)
-            plt.scatter(0.25, filledState, color='black', zorder = 3, s = dotSize)
-            plt.scatter(0.75, emptyState, facecolors='white', edgecolors='black', zorder = 3, s = dotSize)
-            plt.plot([0.5,0.5], [filledState, emptyState], color = 'black', linestyle = 'dashed')
+        defect_name = eigenVal[i - count][3] #Gets the name of the defect from file
+        defect_name = format_label(defect_name)
+        
+        plt.subplot(1, numberOfDefects, plotNum)
+        plt.tick_params(labelbottom=False,bottom=False, top=False, labeltop=False, labelright=False, labelleft=False, left=False, right=False)
+        
+        for j in range(1, len(energy) - 1):
+            if(occupancy[j] < 0.2): #Plots the unoccupied states
+                plt.axhline(energy[j], color = 'black')
+                plt.scatter([0.25, 0.75], [energy[j], energy[j]], facecolors='white', edgecolors='black', zorder = 3, s = dotSize)
+            elif(occupancy[j] > 0.8): #Plots the occupied states
+                plt.axhline(energy[j], color = 'black')
+                plt.scatter([0.25, 0.75], [energy[j], energy[j]], color = 'black', zorder = 3, s = dotSize)
+            else: #Plots the half-occupied states and splits the levels based on ISPIN = 2 Calculation
+                filledState = float(eigenVal[i - (len(energy) - j) - 1][3]) - band_edge
+                emptyState = float(eigenVal[i - (len(energy) - j) - 1][4]) - band_edge
+                plt.axhline(filledState, color = 'black', xmax = 0.5)
+                plt.axhline(emptyState, color = 'black', xmin = 0.5)
+                plt.scatter(0.25, filledState, color='black', zorder = 3, s = dotSize)
+                plt.scatter(0.75, emptyState, facecolors='white', edgecolors='black', zorder = 3, s = dotSize)
+                plt.plot([0.5,0.5], [filledState, emptyState], color = 'black', linestyle = 'dashed')
+        
+        plt.xlabel(defect_name, fontsize = 12)
+        plt.xlim(0,1)
+        plt.ylim(0, energy[len(energy) - 1])
+        
+        energy, occupancy = [], []
+        count = 0
+        plotNum = plotNum + 1
+        
+        if(i > len(eigenVal) - 1):
+            break
+    if(count != 0):
+        energy.append(float(eigenVal[i][1]) - band_edge)
+        occupancy.append(float(eigenVal[i][2]))
     
-    plt.xlabel(title, fontsize = 12)
-    plt.xlim(0,1)
-    plt.ylim(0, band[len(band) - 1] - vbm)
-    k = k + 1
-    
-    band = []
-    occupancy = []
-    
-    i = i + 1
-    total = total + 1
-
-plt.savefig("eigenvalplot.png")
-plt.show()     
+del(count, data, f, i, j, defect_name, dotSize, energy, occupancy, emptyState, filledState)
